fe20e2e3d24ae46bc155b347dcfb805a21de35e3
Daniel P. Egan <dp.egan@gmail.com>
2018-09-30 15:50:02 -0400

    comment out fintwit quotes from schedule.

--- R/buildSchedule.R
+++ R/buildSchedule.R
@@ -7,61 +7,61 @@
 
 
 buildSchedule <- function(){
-  schedule <- data.frame(minute=numeric(), 
-                         hour=numeric(), 
-                         dow=numeric(), 
-                         category=character(), 
+  schedule <- data.frame(minute=numeric(),
+                         hour=numeric(),
+                         dow=numeric(),
+                         category=character(),
                          id=character())
-  
-  schedule <- rbind(schedule, 
-                    data.frame(minute   = 05, 
-                               hour     = 12, 
-                               dow      = 1, 
+
+  schedule <- rbind(schedule,
+                    data.frame(minute   = 05,
+                               hour     = 12,
+                               dow      = 1,
                                tz       = "EST",
-                               category = "dpegan_blog_posts", 
+                               category = "dpegan_blog_posts",
                                id       = NA))
-  
-  schedule <- rbind(schedule, 
-                    data.frame(minute   = 05, 
-                               hour     = 12, 
-                               dow      = 5, 
+
+  schedule <- rbind(schedule,
+                    data.frame(minute   = 05,
+                               hour     = 12,
+                               dow      = 5,
                                tz       = "EST",
-                               category = "dpegan_blog_posts", 
+                               category = "dpegan_blog_posts",
                                id       = NA))
-  
-  schedule <- rbind(schedule, 
-                    data.frame(minute   = 05, 
-                               hour     = 08, 
-                               dow      = 00, 
+
+  schedule <- rbind(schedule,
+                    data.frame(minute   = 05,
+                               hour     = 08,
+                               dow      = 00,
                                tz       = "EST",
-                               category = "befi_fails", 
+                               category = "befi_fails",
                                id       = NA))
-  
-  schedule <- rbind(schedule, 
-                    data.frame(minute   = 05, 
-                               hour     = 08, 
-                               dow      = 02, 
+
+  schedule <- rbind(schedule,
+                    data.frame(minute   = 05,
+                               hour     = 08,
+                               dow      = 02,
                                tz       = "EST",
-                               category = "betterment_posts", 
+                               category = "betterment_posts",
                                id       = NA))
-  
-  schedule <- rbind(schedule, 
-                    data.frame(minute   = 05, 
-                               hour     = 08, 
-                               dow      = 04, 
+
+  schedule <- rbind(schedule,
+                    data.frame(minute   = 05,
+                               hour     = 08,
+                               dow      = 04,
                                tz       = "EST",
-                               category = "betterment_posts", 
+                               category = "betterment_posts",
                                id       = NA))
-  
-  schedule <- rbind(schedule, 
-                    data.frame(minute   = 05, 
-                               hour     = 16, 
-                               dow      = 06, 
-                               tz       = "EST",
-                               category = "fintwit_quotes", 
-                               id       = NA))
-  
+
+  # schedule <- rbind(schedule,
+  #                   data.frame(minute   = 05,
+  #                              hour     = 16,
+  #                              dow      = 06,
+  #                              tz       = "EST",
+  #                              category = "fintwit_quotes",
+  #                              id       = NA))
+
   validateSchedule(schedule)
   return(schedule)
-  
+
 }
