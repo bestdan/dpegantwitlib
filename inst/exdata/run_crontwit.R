bdaf4f4adf5bbf5d7dedcb4fa1c97cd64310b722
Daniel P. Egan <dp.egan@gmail.com>
2018-09-30 15:30:04 -0400

    Documenting what tests I'd want. Minor code changes.

--- inst/exdata/run_crontwit.R
+++ inst/exdata/run_crontwit.R
@@ -1,6 +1,6 @@
 #' @title run_crontwit.R
-#' Ensure you have dependencies. 
 
+#' Ensure you have dependencies.
 if(!require(pacman)) install.packages("pacman", repos='http://cran.us.r-project.org')
 pacman::p_load(twitteR, yaml, devtools, slackr)
 
@@ -9,34 +9,42 @@ if(!require(crontwit)){
 }
 
 if(!require(dpegantwitlib)){
-  devtools::install_github("bestdan/dpegantwitlib")  
+  devtools::install_github("bestdan/dpegantwitlib")
 }
 
 library(crontwit)
 library(dpegantwitlib)
+#----------------
 
 # Load paths
 creds_path    <- file.path("src/degan_creds.yaml")
 twitter_creds <- yaml.load_file(creds_path)$twitter
+#----------------
+
 
 # Tell it to auto-cache the credentials
 options("httr_oauth_cache"=TRUE)
 #getOption("httr_oauth_cache")
-setup_twitter_oauth(consumer_key    = twitter_creds$consumer_key, 
-                    consumer_secret = twitter_creds$consumer_secret, 
-                    access_token    = twitter_creds$access_token, 
+setup_twitter_oauth(consumer_key    = twitter_creds$consumer_key,
+                    consumer_secret = twitter_creds$consumer_secret,
+                    access_token    = twitter_creds$access_token,
                     access_secret   = twitter_creds$access_token_secret)
 
 # Load schedule and tweet_db
 schedule <- dpegantwitlib::schedule
 tweet_db <- dpegantwitlib::tweet_db
+#----------------
 
-# Check schedule and post if necessary. 
+
+
+# Check schedule and post if necessary.
 res <- crontwit::checkScheduleAndPost(schedule, tweet_db)
+#----------------
+
 
 
 # Send a slack notification that you ran.
-bb_slack_creds <- yaml.load_file(creds_path)$bb_slack_crontwit 
+bb_slack_creds <- yaml.load_file(creds_path)$bb_slack_crontwit
 
 slackr::slackrSetup(username = bb_slack_creds$username,
                     channel= bb_slack_creds$channel,
@@ -45,6 +53,7 @@ slackr::slackrSetup(username = bb_slack_creds$username,
 if(is.null(res)){
   slackr::slackr_bot("I just ran. No content.")
 } else {
-  slackr::slackr_bot(paste:"I posted this content: ", 
+  slackr::slackr_bot(paste:"I posted this content: ",
                      paste0(res, collapse = ", "))
 }
+#----------------
--- tests/testthat/test-tweet_db.R
+++ tests/testthat/test-tweet_db.R
@@ -1,5 +1,10 @@
 
-test_that("tweedb is ok", {
+test_that("tweetdb is ok", {
   data("tweetdb", envir = environment())
-  message("I really need a test! ")  
+  message("I really need a test!")
+
+  # What would this test?
+  # That every tweet in the DB has a schedule point?
+  # that every schedule has tweets?
+  # Anything else?
 })
